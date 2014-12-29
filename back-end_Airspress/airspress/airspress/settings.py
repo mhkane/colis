9fe76cf75f989c693a4062495545bcbadf7d60f5
unknown <konoufo@hotmail.fr>
2014-12-29 00:27:51 -0500

    login fb and cleaned site files

---
+++ back-end_Airspress/airspress/airspress/settings.py
@@ -0,0 +1,118 @@
+"""
+Django settings for airspress project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+from authomatic.providers import oauth2, oauth1, openid
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '-okq_pdj$l@u%#fe8ghxdutg=&68*j-+q+zoqr*6%(e%)mb_t8'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# TEMPLATEs directory
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'signup',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'airspress.urls'
+
+WSGI_APPLICATION = 'airspress.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.dummy',
+        'NAME': '',
+        'USER':'',
+        'PASSWORD':'',
+        'HOST':'',
+        'PORT':''
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'America/Montreal'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+
+# PARSE APPLICATION KEYS
+APPLICATION_ID = "9################"
+REST_API_KEY = "a###########################"
+MASTER_KEY = "w################################"
+#FACEBOOK login CONFIG
+CONFIG = {
+          
+    'fb': {
+           
+        'class_': oauth2.Facebook,
+        #unique key for serialization
+        'id':1,
+        # Facebook is an AuthorizationProviders so it needs that
+        'consumer_key': '1537229933223161',
+        'consumer_secret': '5f199d47d0bd5ea9f7f9a6c379a4d139',
+        
+        # But it is also an OAuth 2.0 provider and it needs scope.
+        'scope': ['user_about_me', 'email', 'publish_stream'],
+    },
+    
+    'oi': {
+           
+        # OpenID provider, we don't need that for now.
+        'class_': openid.OpenID,
+        'id':2,
+    }
+}
---
+++ back-end_Airspress/airspress/signup/views.py
@@ -0,0 +1,62 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+from authomatic import Authomatic
+from authomatic.adapters import DjangoAdapter
+from schemes import User
+from airspress.settings import CONFIG
+
+authomatic = Authomatic(CONFIG, 'a super secret random string about falconpress and his brethren')
+
+def home(request):
+    # Create links  to the Login handler.
+    return HttpResponse('''
+        <p align="center">Login with <a href="login/fb">Facebook</a>.<p>
+    ''')
+
+def login(request, provider_name):
+    # We we need the response object for the adapter.
+    response = HttpResponse()
+    
+    # Start the login procedure.
+    result = authomatic.login(DjangoAdapter(request, response), provider_name)
+     
+    # If there is no result, the login procedure is still pending.
+    # Don't write anything to the response if there is no result!
+    if result:
+        # If there is result, the login procedure is over and we can write to response.
+        response.write('<a href="..">Home</a>')
+        
+        if result.error:
+            # Login procedure finished with an error.
+            response.write('<h2>Damn that error: {0}</h2>'.format(result.error.message))
+        
+        elif result.user:
+            
+            # OAuth 2.0 and OAuth 1.0a provide only limited user data on login,
+            # We need to update the user to get more info.
+            if not (result.user.name and result.user.id):
+                result.user.update()
+            
+            # Recover information for Parse authData
+            response.write(u'<h1>Hi {0}</h1>'.format(result.user.name))
+            fbID = result.user.id
+            # response.write(u'<h2>Your email is: {0}</h2>'.format(result.user.email))
+            
+            # Seems like we're done, but there's more we can do...
+            if result.user.credentials:
+                if result.provider.name == 'fb':
+                    #serialized_credentials = result.user.credentials.serialize()
+                    credentials = authomatic.credentials(result.user.credentials)
+                    valid = credentials.valid
+                    expiration_date = credentials.expiration_date
+                    access_token = credentials.token
+                    if valid:
+                        authData = {"facebook": {"id": fbID, "access_token": access_token,
+                         "expiration_date": expiration_date}}
+                        account = User.login_auth(authData)
+                else:
+                    pass
+                
+    return response            
+            # We can acces other things about user with credentials
