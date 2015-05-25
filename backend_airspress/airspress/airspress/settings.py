2f2e3f9e8a2eb7505bf74d1076fb71935964627c
unknown <konoufo@hotmail.fr>
2015-05-25 14:13:58 -0400

    restored table items style on deal-page

--- backend_airspress/airspress/airspress/settings.py
+++ backend_airspress/airspress/airspress/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from django.conf import global_settings
 from authomatic.providers import oauth2
 from django.conf.global_settings import SESSION_ENGINE
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -22,24 +23,28 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'spb_fb$xf-vyc&*^n_^ur2#mgjk32!#+3cmvs&0mr70h!=_0@2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = True
+try:
+    from airspress.local_settings import PRODUCTION
+except:
+    pass
 
+DEBUG = True
+COMPRESS_ENABLED = True
 TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
-# MEDIA URL
-'''
-TEMPLATE_CONTEXT_PROCESSORS = (
 
-  "django.core.context_processors.media",
+TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
+    "django.core.context_processors.request",
 )
-'''
-STATICFILES_FINDERS = ( 
+STATICFILES_FINDERS = (
                        'django.contrib.staticfiles.finders.FileSystemFinder',
                        'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+                        'compressor.finders.CompressorFinder',
                        )
 #Uploaded Files
-FILE_UPLOAD_DIR = 'c:/'
+FILE_UPLOAD_DIR = '/home/konoufo/uploads'
 # TEMPLATEs directory
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 # Application definition
@@ -51,12 +56,16 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'compressor',
     'south',
     'signup',
     'trips',
     'account',
     'paypalrestsdk',
     'asp_payment',
+    'texto_airspress',
+    'custom_tags',
+    'session_security',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -66,6 +75,9 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'airspress.middlewares.AjaxRedirect',
+    'session_security.middleware.SessionSecurityMiddleware',
+
 )
 
 ROOT_URLCONF = 'airspress.urls'
@@ -75,21 +87,24 @@ WSGI_APPLICATION = 'airspress.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'airspress',
-        'USER': 'postgres',
-        'PASSWORD': '#####',
-        'HOST': '',
-        'PORT':'5432',
+if not PRODUCTION:
+    #Dev Database
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'airspress',
+            'USER': 'postgres',
+            'PASSWORD': 'Shinsekai',
+            'HOST': '',
+            'PORT':'5432',
+        }
     }
-}
-
-SOUTH_DATABASE_ADAPTERS = {
-  'default': 'south.db.postgresql_psycopg2'
-}
+    SOUTH_DATABASE_ADAPTERS = {
+      'default': 'south.db.postgresql_psycopg2'
+    }
+else:
+    # Production Database
+    from prod_settings import DATABASES
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
@@ -105,27 +120,28 @@ USE_L10N = True
 USE_TZ = True
 DATE_INPUT_FORMATS = ['%m-%d-%Y','%d-%m-%Y','%Y-%m-%d',
                       ]
-# mEDIA FILES User-uploaded ones
+# MEDIA FILES User-uploaded
 MEDIA_URL = '/media/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_ROOT = '/home/konoufo/airspress/static'
 STATIC_URL = '/static/'
 #Paypal Credentials
 PAYPAL_MODE='sandbox' #or 'live'
-PAYPAL_ID = '#####'
-PAYPAL_SECRET ='#####'
+PAYPAL_ID = 'AasK0xBGMLG8RARnY-SwXrdw_hCADnC2qCYjoPaNHDWIJSrcPvF0J10eyT4t'
+PAYPAL_SECRET ='EJqwBRDBF932t3jOO--kd23etAlxcFS-3Tys717xJkW7s1CPdU34ThvQo4bv'
 # PARSE APPLICATION KEYS
-APPLICATION_ID = "####"
-REST_API_KEY = "a###"
-MASTER_KEY = "wz###"
+APPLICATION_ID = ""
+REST_API_KEY = ""
+MASTER_KEY = ""
 #FACEBOOK login CONFIG
-consumer_secret = '5f#######'
-consumer_key = '15########'
+consumer_secret = '5f199d47d0bd5ea9f7f9a6c379a4d139'
+consumer_key = '1537229933223161'
 CONFIG = {
-          
+
     'fb': {
-           
+
         'class_': oauth2.Facebook,
         'id':1,
         # Facebook is an AuthorizationProvider too.
@@ -134,11 +150,16 @@ CONFIG = {
         # But it is also an OAuth 2.0 provider and it needs scope.
         'scope': ['user_about_me', 'email', 'publish_stream'],
     },
-    
+
 }
 
 #SESSIONS Settings
 SESSION_ENGINE = "django.contrib.sessions.backends.db"
 SESSION_COOKIE_HTTPONLY = True
-SESSION_COOKIE_AGE = 1800
-#SESSION_COOKIE_SECURE = True #Only on Production
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+SESSION_SECURITY_WARN_AFTER = 1200
+SESSION_SECURITY_EXPIRE_AFTER = 1380
+#SESSION_COOKIE_SECURE = True #Only in Production
+if not PRODUCTION:
+    FILE_UPLOAD_DIR = 'C:/Users/-/Downloads/Poject/backend/airspress/media'
+    STATIC_ROOT = 'c:/Users/-/Downloads/Poject/backend/airspress/static'
---
+++ backend_airspress/airspress/static/CACHE/js/a46e3c3eaf65.js
@@ -0,0 +1,22 @@
+function getDateTime(){var now=new Date();var year=now.getFullYear();var month=now.getMonth()+1;var day=now.getDate();var hour=now.getHours();var minute=now.getMinutes();var second=now.getSeconds();if(month.toString().length==1){var month='0'+month;}
+if(day.toString().length==1){var day='0'+day;}
+if(hour.toString().length==1){var hour='0'+hour;}
+if(minute.toString().length==1){var minute='0'+minute;}
+if(second.toString().length==1){var second='0'+second;}
+var dateTime=day+'/'+month+'/'+year+' '+hour+':'+minute+':'+second;return dateTime;};window.onload=function(){var ref=new Firebase("https://radiant-torch-2520.firebaseio.com/data/airdeals/messages/955CXmoq3f");var ref2=new Firebase("https://radiant-torch-2520.firebaseio.com/data/directMessaging/conversations/");var AUTH_TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE0MzI1NzIzNDAsImQiOnsic291cmNlX2lkIjoiOTU1Q1htb3EzZiIsInNvdXJjZSI6ImRlYWxzIiwidXNlcl9pZCI6IkptenFmdExDTDciLCJ1aWQiOiJjdXN0b206Sm16cWZ0TENMNyJ9LCJ2IjowfQ.ynoSR0UYOQ2XAPm4fCPgWVkzx0KG5_DvJ_fN4tpN6AU";ref.unauth();ref.authWithCustomToken(AUTH_TOKEN,function(error,authData){if(error){console.log("Login Failed!",error);}else{console.log("Login Succeeded!",authData);}});ref2.authWithCustomToken(AUTH_TOKEN,function(error,authData){if(error){console.log("Inbox Failed!",error);}else{console.log("Inbox Succeeded!",authData);}});document.getElementById("WrapperConversation1").style.maxHeight=(this.innerHeight-80)+'px';this.onresize=function(){document.getElementById("WrapperConversation1").style.maxHeight=(this.innerHeight-80)+'px';};document.getElementById('SendMessage1').onclick=function(e){e.preventDefault();var newMessageText=document.getElementById('NewMessage1').value;if(newMessageText!==""){ref.push({"sender":"JmzqftLCL7","text":newMessageText,"pub_date":getDateTime()});ref2.update({"inbox":{"senderId":"JmzqftLCL7","senderName":"mohamed.coulibali.1@ulaval.ca","text":newMessageText,"pub_date":Firebase.ServerValue.TIMESTAMP}});document.getElementById('NewMessage1').value='';}};document.getElementById('NewMessage1').onkeydown=function Shidori(e){if(e.keyCode==13||e.which==13){e.preventDefault();var newMessageText=document.getElementById('NewMessage1').value;if(newMessageText!==""){ref.push({"sender":"JmzqftLCL7","text":newMessageText,"pub_date":getDateTime()});ref2.update({"inbox":{"senderId":"JmzqftLCL7","senderName":"mohamed.coulibali.1@ulaval.ca","text":newMessageText,"pub_date":Firebase.ServerValue.TIMESTAMP}});document.getElementById('NewMessage1').value='';}}};ref.limitToLast(10).on("child_added",function(snapshot){var newMessage=snapshot.val();var conversation=document.getElementById('Conversation1');var wrapper=document.getElementById('WrapperConversation1');var snippet='';var part1='<div class="conversation-item item-left clearfix">\
+						<div class="conversation-user">\
+							<img class="hidden-xs" src="  http://files.parsetfss.com/ac36d662-47c9-4a1b-a5da-602509e36f9f/tfss-238c63bc-4ce2-46ae-a2da-a6c621f72245-pic1.jpg  " alt=""/>\
+						</div>\
+						<div class="conversation-body">\
+							<div class="name hidden-xs">  mhkane@mit.edu  </div>\
+							<div class="time hidden-xs">';var part11='<div class="conversation-item item-right clearfix">\
+						<div class="conversation-user">\
+							<img class="hidden-xs" src="  http://files.parsetfss.com/ac36d662-47c9-4a1b-a5da-602509e36f9f/tfss-3dd74684-4a9e-46c6-9070-45dffcfc3be8-pic1.jpg  " alt=""/>\
+						</div>\
+						<div class="conversation-body">\
+							<div class="name hidden-xs">  mohamed.coulibali.1@ulaval.ca  </div>\
+							<div class="time hidden-xs">';var part2='</div>\
+							<div class="text">';var part3='</div>\
+						</div>\
+					</div>';if(newMessage.sender=="JmzqftLCL7"){snippet=part11+newMessage.pub_date+part2+newMessage.text+part3;}else{snippet=part1+newMessage.pub_date+part2+newMessage.text+part3;}
+conversation.innerHTML+=snippet;conversation.scrollTop=conversation.scrollHeight;wrapper.scrollTop=wrapper.scrollHeight;wrapper.style.maxHeight=window.innerHeight;document.getElementById("WrapperConversation1").scrollIntoView(false);});};
