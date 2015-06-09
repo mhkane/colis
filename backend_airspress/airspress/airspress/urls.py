f95cebe9b5ccc43fe94e72e836db10877bb4de93
unknown <konoufo@hotmail.fr>
2015-06-09 03:58:27 -0400

    fixed bug with POSTs due to internationaliz..

--- backend_airspress/airspress/airspress/urls.py
+++ backend_airspress/airspress/airspress/urls.py
@@ -6,7 +6,19 @@ from django.conf.urls.i18n import i18n_patterns
 admin.autodiscover()
 
 
-urlpatterns = patterns('', url(r'session_security/', include('session_security.urls')),)
+urlpatterns = patterns('', 
+   url(r'session_security/', include('session_security.urls')),
+   url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^polls/', include('polls.urls',namespace='polls')),
+    url(r'^', include('signup.urls',namespace='signup')),
+    url(r'^trips/', include('trips.urls',namespace='trips')),
+    url(r'^account/', include('account.urls',namespace='account')),
+    url(r'^payment/', include('asp_payment.urls',namespace='payment')),
+    url(r'^contact/',views.contact, name='contact'),
+    url(r'^faq/',views.faq, name='faq'),
+    url(r'^about/',views.about, name='about'),
+    url(r'^terms/',views.terms, name='terms'),)
 urlpatterns += i18n_patterns('',
     # Examples:
     #url(r'^$', 'airspress.views.home', name='home'),
